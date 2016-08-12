219d821be08d0b1e7bee7448bfef68eb6a0fb9ad
lauranovak <lauranovak@ngi.it>
2016-08-12 09:27:31 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying the footer.
+ * Contains the closing of the id #maincontentcontainer div and all content after.
+ * There are also four footer widgets displayed. These will be displayed from
+ * one to four columns, depending on how many widgets are active.
+ *
+ * @package Lora
+ * @since Lora 1.0
+ */
+?>
