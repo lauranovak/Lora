19abc3688ee743e89503761f6e81ae2ab2fbc3c1
lauranovak <lauranovak@ngi.it>
2016-08-12 09:23:51 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Lora
+ * @since Lora 1.0
+ */
